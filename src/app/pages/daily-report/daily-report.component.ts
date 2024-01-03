278785d5396b101ae371b104cc62de4121d18f31
Gabriel Torres <gabrieltrn.dev@gmail.com>
2024-01-03 11:15:30 -0300

    melhorias com ações de voltar

--- src/app/pages/daily-report/daily-report.component.ts
+++ src/app/pages/daily-report/daily-report.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-daily-report',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './daily-report.component.html',
   styleUrl: './daily-report.component.css'
 })
